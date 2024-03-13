fd412eb25eff8f63aa1fd48747c269b51417dcc9
nico <156666258+nicoDHBW@users.noreply.github.com>
2024-03-13 18:26:07 +0100

    Test

---
+++ code_4_exam/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\033[4;92m========================\n");
+    printf("Choose your action: \n");
+    printf("List Developers \t[1]\n");
+    printf("Print Group Logo \t[2]\n");
+    printf("Print Group \t\t[3]\n");
+    printf("Exit \t\t\t[4]\n");
+    printf("========================\033[0m\n");
+
+    printf("Enter your selection: ");
+}
