99b9e013db2563ba99cf8e3b6ae0f164b0b8b396
nico <156666258+nicoDHBW@users.noreply.github.com>
2024-03-13 19:41:12 +0100

    1.Versuch developer group

---
+++ code_4_exam/include/developer_group.h
@@ -0,0 +1,21 @@
+#ifndef DEVELOPER_GROUP_H
+#define DEVELOPER_GROUP_H
+
+#define MAX_DEVELOPERS 10
+#define MAX_LOGO_LENGTH 50
+
+#include "developer.h" // Include the developer structure
+
+// Structure for a group of developers
+typedef struct {
+    Developer developers[MAX_DEVELOPERS]; // Array of developers
+    char logo[MAX_LOGO_LENGTH];           // Group logo
+} DeveloperGroup;
+
+// Initialization function for a developer group
+void initialize_developer_group(DeveloperGroup *group, char *logo);
+
+// Function to print developers in the group
+void print_developers(DeveloperGroup group);
+
+#endif /* DEVELOPER_GROUP_H */
