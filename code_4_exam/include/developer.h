ff1ee25c652455a2571c67ffa50e0012a499af23
nico <156666258+nicoDHBW@users.noreply.github.com>
2024-03-13 19:16:33 +0100

    Änderung developer.c

---
+++ code_4_exam/include/developer.h
@@ -0,0 +1,20 @@
+#ifndef DEVELOPER_H_
+#define DEVELOPER_H_
+
+#define DEVELOPER_MAX_NAME_LENGTH (16U) //16 bc of the longest name in our group
+#define ALIAS_MAX_NAME_LENGTH (9U)  //9 bc of the longest alias in the group
+
+
+// Structure for a developer
+typedef struct {
+    char name[DEVELOPER_MAX_NAME_LENGTH];  // Name of the developer
+    char alias[ALIAS_MAX_NAME_LENGTH];  // Alias of the developer
+} Developer;
+
+// Initialization function for a developer
+void initialize_developer(Developer *dev, const char *name, const char *alias);
+
+//print the Developer structure to the stdout
+void developer_print(Developer const *dev);
+
+#endif /* DEVELOPER_H_ */
--- code_4_exam/src/developer.c
+++ code_4_exam/src/developer.c
@@ -1,1 +1,36 @@
 #include "developer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Initialization function for a developer
+void initialize_developer(Developer *const dev, const char *name, const char *alias) 
+{
+    
+    // Copy the name into the "name" field of the structure
+    strncpy(dev->name, name, sizeof(dev->name)-1);
+    dev->name[sizeof(dev->name)-1] = '\0'; // Ensure the name is terminated with null terminator
+    
+    // Copy the alias into the "alias" field of the structure
+    strncpy(dev->alias, alias, sizeof(dev->alias)-1);
+    dev->alias[sizeof(dev->alias)-1] = '\0'; // Ensure the alias is terminated with null terminator
+}
+ 
+ 
+ void developer_print(developer const *const dev)
+{
+    if (dev == NULL)
+    {
+        printf("\033[4;33mdeveloper print: invalid data\n\033[0m");
+    }
+    else
+    {
+        printf("=====================\n");
+        printf("DEVELOPER: \n");
+        
+        printf("Name: %s\n", dev->name);
+        printf("Alias: %u\n", dev->alias);
+        printf("=====================\n");
+    }
+}
